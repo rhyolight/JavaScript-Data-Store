bb72120f81be0304872fbdec27fead5a2a7a62c1
Matthew Taylor <rhyolight@gmail.com>
2010-07-20 13:06:55 -0700

    working on events

--- jsds.js
+++ jsds.js
@@ -1,16 +1,121 @@
 JSDS = {
-    
-    _stores: {},
-    
+	
+	_stores: {},
+	
 	create: function(id) {
-		var s = {},
-		    id = id || _randomId();
-		
-		if (this._stores[id]) {
-            throw new Error('Cannot overwrite existing data store "' + id + '"!');
+	    
+	    var jsds = this;
+	    
+        id = id || _randomId();
+
+		function _randomId() {
+			var text = "",
+				i=0,
+				possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+			for(; i < 10; i++ ) {
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
+			}
+			return text;
 		}
 		
-		this._stores[id] = s;
+		/************************************************************************
+		/* The JSDataStore class 
+		/************************************************************************/
+		function JSDataStore(id) {
+		    this.s = {};
+		    this.listeners = {};
+		    this.id = id;
+		}
+		// public methods
+		JSDataStore.prototype.getId = function() {
+		    return this.id;
+		};
+		JSDataStore.prototype.store = function(key, val) {
+            var result;
+            
+			function _store(store, key, val, oldVal /* optional */) {
+                var result, keys, prevKey, currStore, oldKey, oldVal;
+                if (key.indexOf('\.') >= 0) {
+                    keys = key.split('.');
+                    oldVal = store[keys[0]] ? _clone(store[keys[0]]) : undefined;
+                    store[keys[0]] = {};
+                    oldKey = keys.shift();
+                    return _store(store[oldKey], keys, val, oldVal);
+                }
+                result = oldVal ? oldVal[key] : store[key];
+                store[key] = val;
+                return result;
+            }
+            
+            result = _store(this.s, key, val);
+            _fire('store', this, this.listeners);
+            return result;
+		};
+		JSDataStore.prototype.get = function(key) {
+			var s = this.s, keys, i=0, j=0, v;
+			
+			if (arguments.length === 1 && key.indexOf('\.') < 0) {
+				return s[key];
+			}
+			
+			if (arguments.length > 1) {
+				keys = [];
+				for (i=0; i<arguments.length; i++) {
+					if (arguments[i].indexOf('\.') > -1) {
+						var splitKeys = arguments[i].split('\.');
+						for (j=0; j<splitKeys.length; j++) {
+							keys.push(splitKeys[j]);
+						}
+					} else {
+						keys.push(arguments[i]);
+					}
+				}
+			} else if (key.indexOf('\.') > -1) {
+				keys = key.split('\.');
+			}
+			
+			return _getValue(s, keys);
+		};
+		JSDataStore.prototype.on = function(type, fn) {
+		    if (!this.listeners[type]) {
+		        this.listeners[type] = [];
+		    }
+		    this.listeners[type].push(fn);
+		};
+		JSDataStore.prototype.clear = function() {
+			this.s = {};
+		};
+		JSDataStore.prototype.remove = function() {
+		    this.clear();
+		    delete jsds._stores[this.getId()];
+		};
+		// private methods
+		function _fire(type, store, listeners) {
+		    var i, ls = listeners[type];
+		    if (!ls) { return ; }
+		    for (i=0; i<ls.length; i++) {
+		        ls[i](type, store);
+		    }
+		}
+		function _clone(val) {
+			var newObj, i, prop;
+			if (val instanceof Array) {
+				newObj = [];
+				for (i=0; i<val.length; i++) {
+					newObj[i] = _clone(val[i]);
+				}
+			} else if (typeof val === 'object'){
+				newObj = {};
+				for (prop in val) {
+					if (val.hasOwnProperty(prop)) {
+						newObj[prop] = _clone(val[prop]);
+					}
+				}
+			} else {
+				return val;
+			}
+			return newObj;
+		}
 		
 		function _getValue(store, keys) {
 			var key = keys.shift();
@@ -20,94 +125,24 @@ JSDS = {
 				return store[key];
 			}
 		}
+        /************************************************************************
+		/* End of JSDataStore class
+		/************************************************************************/
 		
-        function _clone(val) {
-            var newObj, i, prop;
-            if (val instanceof Array) {
-                newObj = [];
-                for (i=0; i<val.length; i++) {
-                    newObj[i] = _clone(val[i]);
-                }
-            } else if (typeof val === 'object'){
-                newObj = {};
-                for (prop in val) {
-                    if (val.hasOwnProperty(prop)) {
-                        newObj[prop] = _clone(val[prop]);
-                    }
-                }
-            } else {
-                return val;
-            }
-            return newObj;
-        }
-		
-		function _store(store, key, val, oldVal /* optional */) {
-		    var result, keys, prevKey, currStore, oldKey, oldVal;
-		    if (key.indexOf('\.') >= 0) {
-		        keys = key.split('.');
-		        oldVal = store[keys[0]] ? _clone(store[keys[0]]) : undefined;
-		        store[keys[0]] = {};
-		        oldKey = keys.shift();
-		        return _store(store[oldKey], keys, val, oldVal);
-		    }
-		    result = oldVal ? oldVal[key] : store[key];
-			store[key] = val;
-			return result;
+		if (this._stores[id]) {
+			throw new Error('Cannot overwrite existing data store "' + id + '"!');
 		}
-		
-		function _randomId() {
-    	    var text = "",
-    	        i=0,
-    	        possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    	    for(; i < 10; i++ ) {
-    	        text += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-    	    return text;
-    	}
-		
-		return {
-			getId: function() {
-				return id;
-			},
-			store: function(key, val) {
-			    return _store(s, key, val);
-			},
-			get: function(key) {
-				var keys, i=0, j=0, v;
-				
-				if (arguments.length === 1 && key.indexOf('\.') < 0) {
-					return s[key];
-				}
-				
-				if (arguments.length > 1) {
-					keys = [];
-					for (i=0; i<arguments.length; i++) {
-						if (arguments[i].indexOf('\.') > -1) {
-							var splitKeys = arguments[i].split('\.');
-							for (j=0; j<splitKeys.length; j++) {
-								keys.push(splitKeys[j]);
-							}
-						} else {
-							keys.push(arguments[i]);
-						}
-					}
-				} else if (key.indexOf('\.') > -1) {
-					keys = key.split('\.');
-				}
-				
-				return _getValue(s, keys);
-			},
-			clear: function() {
-			    s = {};
-			}
-		};
+
+        this._stores[id] = new JSDataStore(id);
+        
+		return this._stores[id];
 	},
 	
-    get: function(id) {
-        return this._stores[id];
-    },
-    
-    clear: function() {
-        this._stores = {};
-    }
+	get: function(id) {
+		return this._stores[id];
+	},
+	
+	clear: function() {
+		this._stores = {};
+	}
 };
