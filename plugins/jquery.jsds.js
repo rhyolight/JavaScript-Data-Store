7a10158be68da5d2ffeed4a69c0c8ddfccfeac00
Matthew Taylor <rhyolight@gmail.com>
2010-08-02 22:58:35 -0700

    Starting creating a jquery plugin

---
+++ plugins/jquery.jsds.js
@@ -0,0 +1,28 @@
+(function() {
+    var ajax = jQuery.ajax;
+    
+    jQuery.ajax = function(settings) {
+        var store;
+        if (settings.jsds && settings.jsds.id) {
+            settings.success = function(data, status, req) {
+                store = JSDS.get(settings.jsds.id);
+                if (!store) {
+                    store = JSDS.create(settings.jsds.id);
+                }
+                store.store('defaultKey', data, {update: true});
+            };
+            settings.error = function(req, status, err) {
+                
+            };
+            settings.dataType = 'json';
+        }
+        ajax(settings);
+    };
+    
+    jQuery.jsds = {
+        on: function (type, opts) {
+            JSDS.on('defaultKey.' + type, opts);
+        }
+    };
+    
+})();
