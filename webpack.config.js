1ab76c9a27a2d3d5d4d9d55ff0cd598a9642105b
Matthew Taylor <matt@numenta.org>
2018-04-17 15:43:32 -0700

    Adding webpack for some tests

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+"use strict";
+let path = require("path")
+let fs = require("fs")
+
+let pkg = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "package.json"), "utf-8")
+)
+let version = pkg.version
+
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        "./src/jsds.js",
+    ],
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, "src"),
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            SdrUtils: path.join(__dirname, "node_modules/cell-viz/src/SdrUtils"),
+            SdrDrawing: path.join(__dirname, "node_modules/cell-viz/src/SdrDrawing")
+        }
+    },
+    output: {
+        path: __dirname + "/docs",
+        filename: `jsds-${version}.js`
+    }
+}
